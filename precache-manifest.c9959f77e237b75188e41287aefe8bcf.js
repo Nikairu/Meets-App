d39e53c0fbbb3929d6a95fa717ffcc4105d07546
Nikairu <diogosilvaoliveirawork@gnail.com>
2021-05-07 23:37:38 +0100

    Updates

---
+++ precache-manifest.c9959f77e237b75188e41287aefe8bcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8661e8df453f523a4130282a324b2e92",
+    "url": "/Nikairu/Meets-App/index.html"
+  },
+  {
+    "revision": "601f3c4bbdfb831d8f3f",
+    "url": "/Nikairu/Meets-App/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "e3363c9da883f4c0d1c8",
+    "url": "/Nikairu/Meets-App/static/js/2.d263d8a3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Nikairu/Meets-App/static/js/2.d263d8a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "601f3c4bbdfb831d8f3f",
+    "url": "/Nikairu/Meets-App/static/js/main.f8cbd72e.chunk.js"
+  },
+  {
+    "revision": "2198fc0240c8bbead616",
+    "url": "/Nikairu/Meets-App/static/js/runtime-main.d41a3b33.js"
+  }
+]);
