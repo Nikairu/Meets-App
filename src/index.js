3fb67a761d6090e166423ad80a9496fd87e91926
Nikairu <diogosilvaoliveirawork@gnail.com>
2021-05-15 17:09:08 +0100

    we PWA now

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as atatus from "atatus-js";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,5 +15,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+//serviceWorker.unregister();
+serviceWorker.register();
 atatus.config("241783d6fec64ff8a6bd6d6bd8c4611e").install();
